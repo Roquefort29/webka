f3985ec1ebda6fa7a945fe9e4477f023c9132f7b
mercurial <dias.nespayev@gmail.com>
2022-05-30 02:59:48 +0600

    feat: replace description with image

--- models/Item.js
+++ models/Item.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const ItemSchema = new mongoose.Schema({
     title: {type: String, required: true},
-    description: {type: String, required: true},
+    image: {type: String, required: true},
     price: {type: Number, required: true},
     category: {type: Number, required: true}
 }, {collection: 'item'})
