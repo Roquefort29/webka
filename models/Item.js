97eb4ce9109bc579f75a4e9a09e3bacaf4c2d945
mercurial <dias.nespayev@gmail.com>
2022-05-30 02:50:15 +0600

    feat: add fields

--- models/Item.js
+++ models/Item.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 
 const ItemSchema = new mongoose.Schema({
-    title: {type: String, required: true, unique: true},
-    description: {type: String, required: true}
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    price: {type: Number, required: true},
+    category: {type: Number, required: true}
 }, {collection: 'item'})
 
 const model = mongoose.model('ItemSchema', ItemSchema)
